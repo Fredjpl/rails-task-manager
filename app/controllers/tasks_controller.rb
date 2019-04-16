78f8a26bbd79f0644e6bd0ab42700dad8733dbd0
PEILI JIANG <1281277805@qq.com>
2019-04-16 16:28:07 +0800

    .

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,45 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task =Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    # no need for app/views/restaurants/update.html.erb
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
